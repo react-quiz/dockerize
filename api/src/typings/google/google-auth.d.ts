cea9a23c4e4322eca6b8c55d8f2796b6a24df860
binhqd <binhqd@gmail.com>
2016-04-22 15:28:05 +0700

    Remove src folder

--- api/src/typings/google/google-auth.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'googleapis' {
-    namespace googleapis {
-        export interface googleapis {
-            calendar(version: string): void;
-        }
-    }
-    var obj: googleapis.googleapis;
-    export = obj;
-}
